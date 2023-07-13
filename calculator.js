b11413d9a3211444ccacd2515360f3a68791550d
= <=>
2023-07-13 15:54:26 +0100

    calculator working

--- calculator.js
+++ calculator.js
@@ -6,10 +6,16 @@ const addButton = document.getElementById("addButton");
 const subtractButton = document.getElementById("subtractButton");
 const multiplyButton = document.getElementById("multiplyButton");
 const divideButton = document.getElementById("divideButton");
-const answerList = document.getElementById('answerList');
+const enterButton = document.getElementById("enterButton");
+const resetButton = document.getElementById("resetButton");
+const clearButton = document.getElementById("clearButton");
+const answerList = document.getElementById("answerList");
 let value1 = 0;
 let value2 = 0;
+
+// Memory
 let answer = 0;
+let output = "";
 
 
 let addToList = message => {
@@ -22,7 +28,7 @@ addButton.onclick = () => {
     value1 = parseInt(input1.value);
     value2 = parseInt(input2.value);
     answer = value1 + value2;
-    addToList(`${input1.value} + ${input2.value} = ${answer}`);
+    output = (`${input1.value} + ${input2.value} = ${answer}`);
 };
 
 
@@ -30,19 +36,34 @@ subtractButton.onclick = (value1, value2) => {
     value1 = parseInt(input1.value);
     value2 = parseInt(input2.value);
     answer = value1 - value2;
-    addToList(`${input1.value} - ${input2.value} = ${answer}`);
+    output = (`${input1.value} - ${input2.value} = ${answer}`);
 };
 
 multiplyButton.onclick = (value1, value2) => {
     value1 = parseInt(input1.value);
     value2 = parseInt(input2.value);
     answer = value1 * value2;
-    addToList(`${input1.value} x ${input2.value} = ${answer}`);
+    output = (`${input1.value} x ${input2.value} = ${answer}`);
 };
 
 divideButton.onclick = (value1, value2) => {
     value1 = parseInt(input1.value);
     value2 = parseInt(input2.value);
     answer = value1 / value2;
-    addToList(`${input1.value} / ${input2.value} = ${answer}`);
+    output = (`${input1.value} / ${input2.value} = ${answer}`);
+};
+
+resetButton.onclick = () => {
+    answer = 0;
+    output = "";
+    document.getElementById('calcForm').reset();
+
+};
+
+enterButton.onclick = () => {
+    addToList(output);
+};
+
+clearButton.onclick = () => {
+    answerList.innerHTML = '';
 };
