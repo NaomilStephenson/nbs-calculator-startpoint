06c048253b87ef644af28410df31e8c704f17dac
= <=>
2023-07-13 14:03:35 +0100

    calculator working

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ## Calculator
 
-- Using the .html file provided or creating your own from scratch, build a working calculator in HTML, CSS and JS.
+- Using the .html file provided or creating your own from scratch, build a working calculator in HTML, CSS and JS /
 - Try adding bootstrap to your calculator.
-- Provide it two inputs for calculations.
-- When loading the calculator script, make sure the script type in your .html file is "module".
-- You must be able to add, subtract, multiply and divide the inputs.
-- Every new result should be written to the page as a new list item in the Output List.
+- Provide it two inputs for calculations /
+- When loading the calculator script, make sure the script type in your .html file is "module" / - why - what does this do?
+- You must be able to add, subtract, multiply and divide the inputs /
+- Every new result should be written to the page as a new list item in the Output List /
 - Upload your work to GitHub.
 
 ### Bonus
--- calculator.js
+++ calculator.js
@@ -1,3 +1,48 @@
 'use strict'
 
-const inputOne = document.getElementById(`input_1`);
+const input1 = document.getElementById(`value1`);
+const input2 = document.getElementById(`value2`);
+const addButton = document.getElementById("addButton");
+const subtractButton = document.getElementById("subtractButton");
+const multiplyButton = document.getElementById("multiplyButton");
+const divideButton = document.getElementById("divideButton");
+const answerList = document.getElementById('answerList');
+let value1 = 0;
+let value2 = 0;
+let answer = 0;
+
+
+let addToList = message => {
+    let i = document.createElement('li');
+    i.innerText = message;
+    answerList.appendChild(i);
+};
+
+addButton.onclick = () => {
+    value1 = parseInt(input1.value);
+    value2 = parseInt(input2.value);
+    answer = value1 + value2;
+    addToList(`${input1.value} + ${input2.value} = ${answer}`);
+};
+
+
+subtractButton.onclick = (value1, value2) => {
+    value1 = parseInt(input1.value);
+    value2 = parseInt(input2.value);
+    answer = value1 - value2;
+    addToList(`${input1.value} - ${input2.value} = ${answer}`);
+};
+
+multiplyButton.onclick = (value1, value2) => {
+    value1 = parseInt(input1.value);
+    value2 = parseInt(input2.value);
+    answer = value1 * value2;
+    addToList(`${input1.value} x ${input2.value} = ${answer}`);
+};
+
+divideButton.onclick = (value1, value2) => {
+    value1 = parseInt(input1.value);
+    value2 = parseInt(input2.value);
+    answer = value1 / value2;
+    addToList(`${input1.value} / ${input2.value} = ${answer}`);
+};
