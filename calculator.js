17dbc715bbb2458907b7effea0921330ceff62f8
= <=>
2023-07-14 09:22:23 +0100

    Comments and updates from code review with Cameron

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
 'use strict'
 
+// Input Values
 const input1 = document.getElementById(`value1`);
 const input2 = document.getElementById(`value2`);
+
+// Buttons
 const addButton = document.getElementById("addButton");
 const subtractButton = document.getElementById("subtractButton");
 const multiplyButton = document.getElementById("multiplyButton");
@@ -9,23 +12,26 @@ const divideButton = document.getElementById("divideButton");
 const enterButton = document.getElementById("enterButton");
 const resetButton = document.getElementById("resetButton");
 const clearButton = document.getElementById("clearButton");
+
+// List
 const answerList = document.getElementById("answerList");
-let value1 = 0;
-let value2 = 0;
 
 // Memory
+let value1 = 0;
+let value2 = 0;
 let answer = 0;
 let output = "";
 
-
+// Functions
 let addToList = message => {
     let i = document.createElement('li');
     i.innerText = message;
-    answerList.appendChild(i);
+    i.classList.add('list-group-item');                                     // Added to match formatting
+    answerList.append(i);
 };
 
 addButton.onclick = () => {
-    value1 = parseInt(input1.value);
+    value1 = parseInt(input1.value);                                        // could also use Number(input1.value);
     value2 = parseInt(input2.value);
     answer = value1 + value2;
     output = (`${input1.value} + ${input2.value} = ${answer}`);
